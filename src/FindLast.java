ade8dd74f60c3b26a3cf793e9638287da10c3f22
Adam Lopata <aalopata@gmail.com>
2018-03-01 17:31:02 -0500

    Add fault to FindLast.java
    Added a fault to break the build.

--- src/FindLast.java
+++ src/FindLast.java
@@ -5,7 +5,7 @@ public class FindLast {
 	public static void main(String[] args) {
 		int[] array = {1, 2, 3, 4, 5, 2};
 		
-		System.out.println(findLast(array, 2));
+		System.out.println(findLast(array, 2, 0));
 	}
 
 	
